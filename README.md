30ca0092bf24821a123242e8286aad872bc5d201
Sammy Lin <sammy@17.media>
2019-08-24 02:03:22 +0800

    addition some files

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
-# Useful commands
+# Buile up Resize Images on the Fly with Amazon S3, AWS Lambda, and Amazon API Gateway via CDK
 
- * `npm run build`   compile typescript to js
- * `npm run watch`   watch for changes and compile
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk synth`       emits the synthesized CloudFormation template
+1.
+```
+cdk deploy
+```
+
+2. Upload
+
+```
+aws s3 cp example/hello.jpg s3://<BucketName>/
+```
+
+3. open origin image
+open browser to http://<BucketName>.s3-website-us-east-1.amazonaws.com/hello.jpg
+
+4. open resize image
+open browser to  http://<BucketName>.s3-website-us-east-1.amazonaws.com/400x400/hello.jpg
+
+5. Clean Up
+```
+cdk destroy
+```
+
+## Reference
+
+* https://github.com/sagidm/s3-resizer
+* https://aws.amazon.com/cn/blogs/compute/resize-images-on-the-fly-with-amazon-s3-aws-lambda-and-amazon-api-gateway/
--- lib/resize.ts-stack.ts
+++ lib/resize.ts-stack.ts
@@ -1,9 +1,80 @@
 import cdk = require('@aws-cdk/core');
+import s3 = require('@aws-cdk/aws-s3');
+import iam = require('@aws-cdk/aws-iam');
+import lambda = require('@aws-cdk/aws-lambda');
+import apigateway = require('@aws-cdk/aws-apigateway');
+
 
 export class ResizeTsStack extends cdk.Stack {
+  readonly api: apigateway.LambdaRestApi
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket_name = 'MyStaticWebsite_test'
+
+    // ===== Lambda
+    const fn = new lambda.Function(this, 'MyFunction', {
+      runtime: lambda.Runtime.NODEJS_8_10,
+      handler: 'index.handler',
+      timeout: cdk.Duration.seconds(60),
+      memorySize: 512,
+      code: lambda.Code.asset('handler')
+    });
+
+    // ===== APIGateWay
+    const publishedGateway = new apigateway.LambdaRestApi(
+     this, "GetEndpoint",
+      {
+        handler: fn
+      }
+    );
+    const backendURL = `${publishedGateway.restApiId}.execute-api.${this.region}.amazonaws.com`
+
+    // S3
+    const websiteBucket = new s3.Bucket(this, bucket_name, {
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'error.html',
+      websiteRoutingRules: [{
+        hostName: backendURL,
+        httpRedirectCode: '307',
+        protocol: s3.RedirectProtocol.HTTPS,
+        replaceKey: s3.ReplaceKey.prefixWith('prod/resize-func?path='),
+        condition: {
+          httpErrorCodeReturnedEquals: '404',
+        }
+      }],
+    });
+    websiteBucket.grantPublicAccess();
+
+    fn.addToRolePolicy(new iam.PolicyStatement({
+        resources: ['arn:aws:logs:*:*:*'],
+        actions: ['logs:CreateLogGroup',
+                  'logs:CreateLogStream',
+                  'logs:PutLogEvents'] }
+    ));
+    fn.addToRolePolicy(new iam.PolicyStatement({
+        resources: [websiteBucket.bucketArn,`${websiteBucket.bucketArn}/*`,],
+        actions: ['s3:PutObject'] }
+    ));
+//
+    fn.addEnvironment('BUCKET', websiteBucket.bucketName);
+    fn.addEnvironment('URL', websiteBucket.bucketWebsiteUrl);
+    let hint = `
+Upload example image via aws s3 cp
+====
+aws s3 cp example/hello.jpg s3://${websiteBucket.bucketName}/
+=====
+
+then open browser to ${websiteBucket.bucketWebsiteUrl}/hello.jpg
+
+or
+
+${websiteBucket.bucketWebsiteUrl}/400x400/hello.jpg
+
+`
+    new cdk.CfnOutput(this, 'Hint', {
+        value: hint
+    })
+    new cdk.CfnOutput(this, 'WebsiteUrl', { value: websiteBucket.bucketWebsiteUrl });
   }
 }
